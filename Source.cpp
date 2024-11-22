a9162a4d6d40ddc5043c8fae09ed866714d98824
Sandratana Malalaniaina <andrianjoharys06@gmail.com>
2024-11-21 20:47:58 -0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -16,36 +16,36 @@ namespace FlightBooking {
     // Function to search a flight by destination name
     int searchFlight(Flight flights[], int size, const string& destination) {
         for (int i = 0; i < size; ++i) {
-            if (flights[i].destination == destination && flights[i].availableSeats > 0) {
+            if (flights[i].destination == destination && flights[i].availableSeats > 0) { // Check if destination matches and seats are available
                 return i;
             }
         }
-        return -1;
+        return -1; //returning -1 if no matching flights found
     }
 
     // Function to book a flight 
    Passenger* bookFlight(Flight& flight) {
-        if (flight.availableSeats <= 0) {
+        if (flight.availableSeats <= 0) { //Check if seats are available
             cout << "No seats available on this flight." << endl;
             return nullptr;
         }
-
+        //// Dynamically allocate memory for a new passenger
         Passenger* newPassenger = new Passenger;
         cout << "Enter passenger name: ";
-        getline(cin, newPassenger->name);
+        getline(cin, newPassenger->name); //getting passenger name
 
-        newPassenger->destination = flight.destination;
+        newPassenger->destination = flight.destination; //setting passenger destination
 
         int seatClassChoice;
         cout << "Choose seat class (0: Economy, 1: Business, 2: First Class): ";
         cin >> seatClassChoice;
-        newPassenger->seatClass = static_cast<SeatClass>(seatClassChoice);
+        newPassenger->seatClass = static_cast<SeatClass>(seatClassChoice); //Assign seat choice
 
-        flight.availableSeats--;
+        flight.availableSeats--; // Decreasing available seats on the flight
         cout << "Booking confirmed for " << newPassenger->name
             << " on flight to " << flight.destination << endl;
 
-        return newPassenger;
+        return newPassenger; // Return pointer to the booked passenger
     }
 
     // Function to display passenger details
